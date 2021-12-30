edf0ae6e2d1e2fc409f53f7fec760c41efdfc99f
RITESH THAKUR <thakurram2668@gmail.com>
2021-12-30 15:33:36 +0530

    added college

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Hyy , I am Ritesh Kumar Thakur
         </p>
+        <p>From BIT DURG </p>
         <a
           className="App-link"
           href="https://reactjs.org"
