f271a0d6cf2ff7076dc10f0fdbb10c28109651b9
RITESH THAKUR <thakurram2668@gmail.com>
2021-12-30 15:25:55 +0530

    added name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hyy , I am Ritesh Kumar Thakur
         </p>
         <a
           className="App-link"
